9b90fcfce1073dda915a19ccfb8da6648f9d599d
zajitschek <64367116+zajitschek@users.noreply.github.com>
2020-04-26 20:13:59 +0100

    Update exc_01_03.R

--- exercises/exc_01_03.R
+++ exercises/exc_01_03.R
@@ -3,9 +3,9 @@ library(ggplot2)
 mtcars$gear <- factor(mtcars$gear,levels=c(3,4,5),
   	labels=c("3gears","4gears","5gears"))
 #mtcars$am <- factor(mtcars$am,levels=c(0,1),
-  	labels=c("Automatic","Manual"))
+#  	labels=c("Automatic","Manual"))
 #mtcars$cyl <- factor(mtcars$cyl,levels=c(4,6,8),
-   labels=c("4cyl","6cyl","8cyl"))
+#   labels=c("4cyl","6cyl","8cyl"))
 
 # Print the gear variable of mtcars
 print(mtcars$gear)
