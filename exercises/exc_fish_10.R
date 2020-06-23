44b003d98b5c2076b0be2c60b1a8dfb0c205c395
zajitschek <64367116+zajitschek@users.noreply.github.com>
2020-06-23 21:53:59 +0100

    Update exc_fish_10.R

--- exercises/exc_fish_10.R
+++ exercises/exc_fish_10.R
@@ -2,6 +2,6 @@
 library(metafor)
 fish.all2 <- read.csv("./binder/data/Macartney2019_effectsize2_fish.csv")
 
-res.mod <- rma.mv(effect_size, variance, mods = ~ trait_category, random = list(~ 1 | study_id, ~ 1 | effectsize_id, ~ 1 | genus), slab= paste(study_id, trait_measured, sep=", "), data = fish.all2)
+res.mod <- rma.mv(effect_size, variance, mods = ~ trait_category -1, random = list(~ 1 | study_id, ~ 1 | effectsize_id, ~ 1 | genus), slab= paste(study_id, trait_measured, sep=", "), data = fish.all2)
 print(res.mod, digits= 2)
 forest(res.mod)
