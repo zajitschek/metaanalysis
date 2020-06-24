40d306f308d18d264d65c8564f8f0844e04b9ca5
zajitschek <64367116+zajitschek@users.noreply.github.com>
2020-06-24 01:41:27 +0100

    ch3 and R code exc9-11

--- exercises/exc_fish_11.R
+++ exercises/exc_fish_11.R
@@ -3,5 +3,6 @@ library(metafor)
 fish.all2 <- read.csv("./binder/data/Macartney2019_effectsize2_fish.csv")
 fish.all2 <- fish.all2 %>% arrange(., trait_category) #re-order by trait category
 
-res.mod.rand <- rma.mv(effect_size, variance, mods = ~ trait_category, random = ~ 1 | study_id, slab= paste(study_id, trait_measured, sep=", "), data = fish.all2)
-funnel(res.mod.rand)
+res.mod <- rma.mv(effect_size, variance, mods = ~ factor(trait_category)-1, random = list(~ 1 | study_id, ~ 1 | effectsize_id, ~ 1 | genus), slab= paste(study_id, trait_measured, sep=", "), data = fish.all2)
+print(res.mod, digits= 2)
+forest(res.mod)
--- exercises/exc_fish_9.R
+++ exercises/exc_fish_9.R
@@ -3,6 +3,6 @@ library(metafor)
 fish.all2 <- read.csv("./binder/data/Macartney2019_effectsize2_fish.csv")
 fish.all2 <- fish.all2 %>% arrange(., trait_category) #re-order by trait category
 
-res.mod.rand <- rma.mv(effect_size, variance, random = list(~ 1 | study_id, ~ 1 | effectsize_id, ~ 1 | genus), slab= paste(study_id, trait_measured, sep=", "), data = fish.all2)
-print(res.mod.rand, digits= 2)
-forest(res.mod.rand)
+res.rand <- rma.mv(effect_size, variance, random = list(~ 1 | study_id, ~ 1 | effectsize_id, ~ 1 | genus), slab= paste(study_id, trait_measured, sep=", "), data = fish.all2)
+print(res.rand, digits= 2)
+forest(res.rand)
